ac2d0a23f2a3a2195c82bd29bdaf92dfddec34c9
Sam Parkinson <parkinsonsp42@gmail.com>
2019-09-06 12:19:42 -0400

    Begin list component creation

--- src/routes/StorePage/StorePage.js
+++ src/routes/StorePage/StorePage.js
@@ -1,6 +1,21 @@
 import React, { Component } from 'react';
+import StoreContext from '../../contexts/StoreContext';
 
 export default class StorePage extends Component {
+  static contextType = StoreContext;
+
+  renderStore() {
+    const { storeItems = [] } = this.context;
+    return storeItems.map((item, index) => 
+      <li
+        key={index}
+      >
+        <h3>{item.name}</h3>
+        <span>Harvest date: {item.harvest_date}</span>
+      </li>
+    )
+  }
+
   render() {
     return (
       <main>
@@ -12,19 +27,19 @@ export default class StorePage extends Component {
               <option>Filter option</option>
               <option>Filter option</option>
             </select>
+            <br />
             <label htmlFor="sort">Sort</label>
             <select id="sort">
               <option>Sort option</option>
               <option>Sort option</option>
             </select>
+            <br />
             <button>Filter</button>
           </form>
         </section>
         <section>  
           <ul>
-            <li>Item name and info</li>
-            <li>Item name and info</li>
-            <li>Item name and info</li>
+            {this.renderStore()}
           </ul>
         </section>
       </main>
