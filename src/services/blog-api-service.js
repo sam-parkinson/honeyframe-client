a32acccada40051b1ca97f89f9ecf64d790fa39f
Sam Parkinson <parkinsonsp42@gmail.com>
2019-09-17 13:34:23 -0400

    Add service object files

---
+++ src/services/blog-api-service.js
@@ -0,0 +1,5 @@
+import config from '../config';
+
+const BlogApiService = {};
+
+export default BlogApiService;
---
+++ src/services/contact-api-service.js
@@ -0,0 +1,5 @@
+import config from '../config';
+
+const ContactApiService = {};
+
+export default ContactApiService;
